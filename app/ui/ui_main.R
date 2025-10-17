203eada6d47311b371c4eb74fec539dd6dd02a8d
jiujiuc-sudo <jiujiuc@student.unimelb.edu.au>
2025-10-17 14:58:05 +1100

    Update ui_main.R

--- app/ui/ui_main.R
+++ app/ui/ui_main.R
@@ -1,10 +1,13 @@
 ui <- navbarPage(
   title = "GEOM90007 Assignment 3",
   id = "nav",
+  header=setUpTableauInShiny(),
   theme = shinythemes::shinytheme("flatly"),
   
   tabPanel("Word Cloud", icon = icon("cloud"), uiOutput("wordcloud_ui")),
   tabPanel("Weather Summary", icon = icon("sun"), uiOutput("weather_ui")),
   tabPanel("Map", icon = icon("map"), uiOutput("map_ui")),
-  tabPanel("Summary", icon = icon("chart-bar"), uiOutput("summary_ui"))
+  tabPanel("Summary", icon = icon("chart-bar"), uiOutput("summary_ui")),
+  main_cloud_tab,
+  sub_cloud_tab
 )
