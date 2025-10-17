d1e6594320d164f058e69f5a618f68bb690e35a0
jiujiuc-sudo <jiujiuc@student.unimelb.edu.au>
2025-10-17 14:21:58 +1100

    Update ui_wordcloud.R

--- app/ui/ui_wordcloud.R
+++ app/ui/ui_wordcloud.R
@@ -1,6 +1,25 @@
-wordcloud_ui <- fluidPage(
-  div(style = "text-align: center; padding: 100px;",
-      h2("Word Cloud"),
-      p("Feature to be implemented")
+# Main Theme Cloud Tab
+main_cloud_tab <- tabPanel(
+  title='Main Themes',
+  h2('Main Theme Word Cloud'),
+  p('Please click on the theme you want to explore in Melbourne', 
+    style='color: #666; font-style: italic; margin-bottom: 10px;'),
+  tableauPublicViz(
+    id='mainCloudViz',       
+    url='https://public.tableau.com/views/mymaincloud/Maincloud',
+    height="85vh"
+  )
+)
+
+# Sub Theme Cloud Tab
+sub_cloud_tab <- tabPanel(
+  title='Sub Themes',
+  h2('Sub Theme Word Cloud'),
+  p('Sub themes related to your selected main theme', 
+    style='color: #666; font-style: italic; margin-bottom: 10px;'),
+  tableauPublicViz(
+    id='subCloudViz',       
+    url='https://public.tableau.com/views/mysubcloud/Subthemecloud',
+    height="85vh"
   )
 )
