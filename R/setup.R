934c4d55a6fc46ea9ea89c80d705009ae3b6bb30
olodii36 <eunkim@student.unimelb.edu.au>
2025-10-17 15:43:32 +1100

    Update setup.R
    added more libraries

--- R/setup.R
+++ R/setup.R
@@ -1,8 +1,10 @@
 # Package installation and loading
 required_packages <- c(
   "shiny", "shinydashboard", "shinyWidgets", "leaflet", "tidyverse", 
-  "here", "sf", "readr", "shinycssloaders", "shinyjs"
-)
+  "here", "sf", "readr", "shinycssloaders", "shinyjs",
+  
+  # megan added
+  "shinythemes", "bslib", "sortable", "lubridate", "stringr", "later")
 
 # Install missing packages
 new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
